cc628a8b87a76769fbac0a49a858cc883e2675a2
MohamedSamy8 <starpes30@gmail.com>
2026-09-17 14:34:20 +0300

    Initial commit - Text Encryption Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Text_Encryption_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtHistroy = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSaveProcess = new System.Windows.Forms.Button();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnCopyResult = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblLength = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblOutput.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblOutput.Location = new System.Drawing.Point(12, 336);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(200, 75);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "Output";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Text_Encryption_Project.Properties.Resources.Text_Encryption_Background;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1105, 761);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(318, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text Encryption";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input Text";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(238, 118);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(413, 116);
+            this.txtInput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(12, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Security Key";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.White;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(238, 336);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(413, 116);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // txtHistroy
+            // 
+            this.txtHistroy.BackColor = System.Drawing.Color.White;
+            this.txtHistroy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHistroy.Location = new System.Drawing.Point(238, 528);
+            this.txtHistroy.Multiline = true;
+            this.txtHistroy.Name = "txtHistroy";
+            this.txtHistroy.ReadOnly = true;
+            this.txtHistroy.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistroy.Size = new System.Drawing.Size(780, 197);
+            this.txtHistroy.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.White;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Black;
+            this.numericUpDown1.Location = new System.Drawing.Point(238, 248);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(94, 45);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(232, 481);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(214, 37);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Histroy Log :";
+            // 
+            // btnSaveProcess
+            // 
+            this.btnSaveProcess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSaveProcess.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveProcess.Location = new System.Drawing.Point(19, 557);
+            this.btnSaveProcess.Name = "btnSaveProcess";
+            this.btnSaveProcess.Size = new System.Drawing.Size(193, 54);
+            this.btnSaveProcess.TabIndex = 10;
+            this.btnSaveProcess.Text = "Save Process";
+            this.btnSaveProcess.UseVisualStyleBackColor = false;
+            this.btnSaveProcess.Click += new System.EventHandler(this.btnSaveProcess_Click);
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.BackColor = System.Drawing.Color.Black;
+            this.btnDeleteAll.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteAll.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDeleteAll.Location = new System.Drawing.Point(19, 635);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(193, 54);
+            this.btnDeleteAll.TabIndex = 11;
+            this.btnDeleteAll.Text = "Delete All";
+            this.btnDeleteAll.UseVisualStyleBackColor = false;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnEncrypt.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(681, 136);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(156, 81);
+            this.btnEncrypt.TabIndex = 12;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.Black;
+            this.btnDecrypt.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDecrypt.Location = new System.Drawing.Point(879, 136);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(156, 81);
+            this.btnDecrypt.TabIndex = 13;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnCopyResult
+            // 
+            this.btnCopyResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCopyResult.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopyResult.Location = new System.Drawing.Point(668, 336);
+            this.btnCopyResult.Name = "btnCopyResult";
+            this.btnCopyResult.Size = new System.Drawing.Size(193, 54);
+            this.btnCopyResult.TabIndex = 14;
+            this.btnCopyResult.Text = "Copy Result";
+            this.btnCopyResult.UseVisualStyleBackColor = false;
+            this.btnCopyResult.Click += new System.EventHandler(this.btnCopyResult_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Black;
+            this.btnClear.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnClear.Location = new System.Drawing.Point(668, 398);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(193, 54);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblLength.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLength.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblLength.Location = new System.Drawing.Point(867, 357);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(212, 54);
+            this.lblLength.TabIndex = 16;
+            this.lblLength.Text = "Length :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 737);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCopyResult);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.btnSaveProcess);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtHistroy);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Text Encryption";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtHistroy;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSaveProcess;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnCopyResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Encryption_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsInputTextBoxEmpty()
+        {
+            return string.IsNullOrEmpty(txtInput.Text);
+        }
+        private bool IsOutputTextBoxEmpty()
+        {
+            return string.IsNullOrEmpty(txtOutput.Text);
+        }
+
+        private bool IsTextBoxesEmpty()
+        {
+            return (IsInputTextBoxEmpty() || IsOutputTextBoxEmpty());
+        }
+        private void ResetTextBoxes()
+        {
+            txtInput.Clear();
+            txtOutput.Clear();
+        }
+        private void ResetLabels()
+        {
+            lblLength.Text = "Length : ";
+            lblOutput.Text = "Output";
+        }
+        
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ResetLabels();
+            ResetTextBoxes();
+        }
+
+        private string EncryptText()
+        {
+            string EncryptedText = "";
+
+            for (int i = 0;i <txtInput.TextLength;i++)
+            {
+                EncryptedText += Convert.ToChar(txtInput.Text[i ]+ Convert.ToInt32(numericUpDown1.Value));
+            }
+
+            return EncryptedText;
+        }
+
+        private string DecryptText()
+        {
+            string DecryptedText = "";
+
+            for (int i = 0; i < txtInput.TextLength; i++)
+            {
+                DecryptedText += Convert.ToChar(txtInput.Text[i] - Convert.ToInt32(numericUpDown1.Value));
+            }
+
+            return DecryptedText;
+        }
+
+        private void GetLength()
+        {
+            lblLength.Text = "Length : " + txtInput.Text.Length;
+        }
+        private void Encrypt()
+        {
+            lblOutput.Text = "Encrypted \nOutput";
+            txtOutput.Text = EncryptText();
+        }
+        private void Decrypt()
+        {
+            lblOutput.Text = "Decrypted \nOutput";
+            txtOutput.Text = DecryptText();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if(!IsInputTextBoxEmpty())
+            {
+                Encrypt();
+
+                GetLength();
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!IsInputTextBoxEmpty())
+            {
+                Decrypt();
+
+                GetLength();
+            }
+
+        }
+
+        private void btnCopyResult_Click(object sender, EventArgs e)
+        {
+            if (!IsOutputTextBoxEmpty())
+            {
+                Clipboard.SetText(txtOutput.Text);
+            }
+        }
+
+        private void btnSaveProcess_Click(object sender, EventArgs e)
+        {
+            if(!IsTextBoxesEmpty())
+            {
+                txtHistroy.Text += " [ " + DateTime.Now.ToString() + " ] ,";
+                txtHistroy.Text += " Input : " + txtInput.Text + " , ";
+                txtHistroy.Text += lblOutput.Text + " : " + txtOutput.Text + " ,";
+                txtHistroy.Text += " Text " + lblLength.Text + Environment.NewLine;
+            }
+        }
+
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            txtHistroy.Clear();
+        }
+    }
+}
